1cc8ee2baa4fcad71e124de88e2dfac32641df23
Set <12975157+thegitparticle@users.noreply.github.com>
2021-12-18 21:56:46 +0530

    bottom sheet which bounces at snap points

--- src/buckets/home/screens/HomeLandingScreen.js
+++ src/buckets/home/screens/HomeLandingScreen.js
@@ -16,6 +16,8 @@ import Iconly from '../../../miscsetups/customfonts/Iconly';
 import {ButterThemeLight, ButterThemeDark} from '../../../theme/ButterTheme';
 import FastImage from 'react-native-fast-image';
 import LottieView from 'lottie-react-native';
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
@@ -24,6 +26,47 @@ const colorScheme = Appearance.getColorScheme();
 const themeHere = colorScheme == 'dark' ? ButterThemeDark : ButterThemeLight;
 
 function HomeLandingScreen({dispatch}) {
+  const renderHeader = () => (
+    <View
+      style={{
+        backgroundColor: 'tomato',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+      }}>
+      <Text style={{...themeHere.text.header, color: 'white'}}>
+        This is Header
+      </Text>
+    </View>
+  );
+
+  const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: 'brown',
+        height: 450,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text style={{...themeHere.text.subhead_medium, color: 'white'}}>
+        Swipe to close
+      </Text>
+    </View>
+  );
+
+  const springConfig = {
+    damping: 500,
+    stiffness: 1000,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 10,
+    restSpeedThreshold: 10,
+  };
+
+  const sheetRef = React.useRef(null);
+
   return (
     <View style={styles.screen_view}>
       <Text>superblack inside bro</Text>
@@ -38,34 +81,51 @@ function HomeLandingScreen({dispatch}) {
           AsyncStorage.clear();
         }}>
         <Text>log out</Text>
-        <LottieView
-          source={require('../../../../assets/loading_ping_pong_cup.json')}
-          style={{width: 200, height: 200}}
-          autoPlay
-          loop
-        />
-        <FastImage
-          style={{width: 200, height: 200}}
-          source={{
-            uri: 'https://unsplash.it/400/400?image=1',
-            headers: {Authorization: 'someAuthToken'},
-            priority: FastImage.priority.normal,
-          }}
-          resizeMode={FastImage.resizeMode.contain}
-        />
-        <Text
-          style={{
-            ...themeHere.text.title_3,
-            color: themeHere.colors.you_prime,
-          }}>
-          this is the custom font
-        </Text>
-        <Button
-          icon={<Icon name="arrow-right" size={15} color="white" />}
-          title="Button with icon component"
-        />
-        <Iconly name="ChevronLeftBroken" color="#000" size={25} />
       </TouchableOpacity>
+      <LottieView
+        source={require('../../../../assets/loading_ping_pong_cup.json')}
+        style={{width: 200, height: 200}}
+        autoPlay
+        loop
+      />
+      <FastImage
+        style={{width: 200, height: 200}}
+        source={{
+          uri: 'https://unsplash.it/400/400?image=1',
+          headers: {Authorization: 'someAuthToken'},
+          priority: FastImage.priority.normal,
+        }}
+        resizeMode={FastImage.resizeMode.contain}
+      />
+      <Text
+        style={{
+          ...themeHere.text.title_3,
+          color: themeHere.colors.you_prime,
+        }}>
+        this is the custom font
+      </Text>
+      <Button
+        icon={<Icon name="arrow-right" size={15} color="white" />}
+        title="Button with icon component"
+      />
+      <Button
+        title="Open Bottom Sheet"
+        onPress={() => sheetRef.current.snapTo(0)}
+      />
+      <Iconly name="ChevronLeftBroken" color="#000" size={25} />
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[550, 300, 0]}
+        renderHeader={renderHeader}
+        renderContent={renderContent}
+        springConfig={{
+          mass: 0.3,
+          damping: 5,
+          stiffness: 80,
+          overshootClamping: false,
+        }}
+        overdragResistanceFactor={2}
+      />
     </View>
   );
 }
