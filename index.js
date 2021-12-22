77a52eb0e5ec7d3401c42b733037ca407cac0be2
Set <12975157+thegitparticle@users.noreply.github.com>
2021-12-22 18:02:14 +0530

    added r-n-crypto and rn-nodeify - and their deps

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
+import './shim.js';
+import crypto from 'crypto';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
