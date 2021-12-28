854716daccf72026ee4dd950cca4940f63ea774f
Set <12975157+thegitparticle@users.noreply.github.com>
2021-12-29 00:17:14 +0530

    my apps, discover apps - redux actions and reducers setup

---
+++ src/redux/DiscoverAppsActions.js
@@ -0,0 +1,23 @@
+import {ADD_DISCOVERAPPS} from './types';
+import axios from 'axios';
+
+export const GetDiscoverApps = user_id => {
+  console.log(user_id, 'user_id passed to actions api calls');
+
+  return dispatch => {
+    let res = [];
+
+    axios
+      .get('https://run.mocky.io/v3/66004f3d-e25e-43e8-a95e-a44a5a614508')
+      .then(response => (res = response.data))
+      .then(() =>
+        dispatch({
+          type: ADD_DISCOVERAPPS,
+          payload: res,
+        }),
+      )
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
---
+++ src/redux/MyAppsActions.js
@@ -0,0 +1,23 @@
+import {ADD_MYAPPS} from './types';
+import axios from 'axios';
+
+export const GetMyApps = user_id => {
+  console.log(user_id, 'user_id passed to actions api calls');
+
+  return dispatch => {
+    let res = [];
+
+    axios
+      .get('https://run.mocky.io/v3/1253d9c6-e54e-448f-abf5-d03fc06d91b0')
+      .then(response => (res = response.data))
+      .then(() =>
+        dispatch({
+          type: ADD_MYAPPS,
+          payload: res,
+        }),
+      )
+      .catch(err => {
+        console.log(err);
+      });
+  };
+};
