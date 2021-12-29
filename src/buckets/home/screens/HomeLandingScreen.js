f9c2a4d28bd321694a5bdb5befaa5e4aa665a5a2
Set <12975157+thegitparticle@users.noreply.github.com>
2021-12-29 14:36:07 +0530

    Home Screen - tabs, labels, focus logic setup

--- src/buckets/home/screens/HomeLandingScreen.js
+++ src/buckets/home/screens/HomeLandingScreen.js
@@ -12,7 +12,7 @@ import {LOGOUT} from '../../../redux/types';
 import {connect} from 'react-redux';
 import {Button} from 'react-native-elements';
 import {ButterThemeLight, ButterThemeDark} from '../../../theme/ButterTheme';
-import {TabView, SceneMap} from 'react-native-tab-view';
+import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 import CryptoPricesPage from '../pages/CryptoPricesPage';
 import HomeMainPage from '../pages/HomeMainPage';
@@ -44,12 +44,83 @@ function HomeLandingScreen({dispatch, navigation}) {
     {key: 'fourth', title: 'DeFi'},
   ]);
 
+  function renderLabel({route, focused}) {
+    if (route.title === 'Market') {
+      if (focused) {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_focused}>Market</Text>
+          </View>
+        );
+      } else {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_unfocused}>Market</Text>
+          </View>
+        );
+      }
+    } else if (route.title === 'Home') {
+      if (focused) {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_focused}>Home</Text>
+          </View>
+        );
+      } else {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_unfocused}>Home</Text>
+          </View>
+        );
+      }
+    } else if (route.title === 'NFTs') {
+      if (focused) {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_focused}>NFTs</Text>
+          </View>
+        );
+      } else {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_unfocused}>NFTs</Text>
+          </View>
+        );
+      }
+    } else {
+      if (focused) {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_focused}>DeFi</Text>
+          </View>
+        );
+      } else {
+        return (
+          <View style={styles.tab_label_view}>
+            <Text style={styles.tab_label_text_unfocused}>DeFi</Text>
+          </View>
+        );
+      }
+    }
+  }
+
+  const renderTabBar = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={styles.tab_bar_indicator}
+      style={styles.tab_bar}
+      renderLabel={renderLabel}
+      tabStyle={{backgroundColor: 'transparent'}}
+    />
+  );
+
   return (
     <View style={styles.parent_view}>
       <HeaderOnHome />
       <TabView
         navigationState={{index, routes}}
         renderScene={renderScene}
+        renderTabBar={renderTabBar}
         onIndexChange={setIndex}
         initialLayout={{width: windowWidth}}
       />
@@ -68,5 +139,39 @@ const styles = StyleSheet.create({
   parent_view: {
     flex: 1,
     backgroundColor: themeHere.colors.background,
+  },
+  tab_bar: {
+    backgroundColor: themeHere.colors.off_background,
+    height: 50,
+    // justifyContent: 'center',
+    alignSelf: 'center',
+    width: windowWidth,
+    borderRadius: 30,
+    borderTopWidth: 0,
+    shadowColor: themeHere.colors.mid_ground,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+    elevation: 9,
+  },
+  tab_bar_indicator: {
+    width: 0,
+  },
+  tab_label_view: {
+    alignItems: 'center',
+    alignContent: 'center',
+    height: 50,
+    justifyContent: 'center',
+  },
+  tab_label_text_focused: {
+    ...themeHere.text.body_medium,
+    color: themeHere.colors.red,
+  },
+  tab_label_text_unfocused: {
+    ...themeHere.text.body_medium,
+    color: themeHere.colors.foreground,
   },
 });
