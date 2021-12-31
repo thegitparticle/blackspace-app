f0878f53e6ac0ba4a17f72579a5dee2945562331
Set <12975157+thegitparticle@users.noreply.github.com>
2021-12-31 22:27:59 +0530

    MiniAppLanding screen opens from bottom - react nav transition is setup

--- src/navigation/MainStack.js
+++ src/navigation/MainStack.js
@@ -20,6 +20,18 @@ const TestMain = createStackNavigator();
 const MyProfileMain = createStackNavigator();
 const SettingsMain = createStackNavigator();
 
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
 function TestStack() {
   return (
     <TestMain.Navigator>
@@ -93,6 +105,11 @@ function HomeAndAppsStack() {
         options={{
           gestureEnabled: true,
           headerShown: false,
+          cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+          transitionSpec: {
+            open: config,
+            close: config,
+          },
         }}
       />
     </HomeAndAppMain.Navigator>
