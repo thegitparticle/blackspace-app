b4f37335d142d0eed20695e65a06f4a029e7cdbc
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-02 02:18:20 +0530

    GlassMorphic Try - added blur library, found a little apt bg for first gradient concept

--- src/buckets/test/GlassBgScreenTest.js
+++ src/buckets/test/GlassBgScreenTest.js
@@ -11,6 +11,7 @@ import {ButterThemeDark, ButterThemeLight} from '../../theme/ButterTheme';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {useNavigation} from '@react-navigation/native';
 import GradientBgBehindGlass from './GradientBgBehindGlass';
+import {BlurView} from '@react-native-community/blur';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
@@ -35,6 +36,12 @@ function GlassBgScreenTest() {
   function Item(item_deets) {
     return (
       <View style={styles.test_item_view}>
+        <BlurView
+          style={styles.test_item_glass_view}
+          blurType="dark"
+          blurAmount={10}
+          reducedTransparencyFallbackColor={themeHere.colors.background}
+        />
         <Text style={{...themeHere.text.subhead_medium, color: 'white'}}>
           {item_deets.title}
         </Text>
@@ -46,11 +53,15 @@ function GlassBgScreenTest() {
     <View style={styles.parent_view}>
       <ImageBackground
         style={styles.behind_glass_image}
-        source={require('../../../assets/color_grad_bg_1.png')}>
-        <View style={styles.glass_view}>
-          <Text style={styles.header_text}>Where to find glass?</Text>
-          {test_screens.map(item => Item(item))}
-        </View>
+        source={require('../../../assets/color_grad_bg_3.png')}>
+        <BlurView
+          style={styles.glass_view}
+          blurType="dark"
+          blurAmount={10}
+          reducedTransparencyFallbackColor={themeHere.colors.background}
+        />
+        <Text style={styles.header_text}>Where to find glass?</Text>
+        {test_screens.map(item => Item(item))}
       </ImageBackground>
     </View>
   );
@@ -75,7 +86,11 @@ const styles = StyleSheet.create({
     height: windowHeight,
     flex: 1,
     alignItems: 'center',
-    backgroundColor: themeHere.colors.background + '50',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
   header_text: {
     ...themeHere.text.title_3,
@@ -87,9 +102,21 @@ const styles = StyleSheet.create({
     width: windowWidth - 40,
     height: 50,
     borderRadius: 15,
-    backgroundColor: themeHere.colors.off_background,
     alignItems: 'center',
     alignSelf: 'center',
     justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  test_item_glass_view: {
+    width: windowWidth - 40,
+    height: 50,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
