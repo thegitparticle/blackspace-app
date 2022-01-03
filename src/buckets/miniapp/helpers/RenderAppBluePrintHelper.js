2cfa81326923d5b4d94c566a9503031f2722148d
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-04 00:43:12 +0530

    MiniAppLanding - render function for blueprint of all - helper is added

--- src/buckets/miniapp/helpers/RenderAppBluePrintHelper.js
+++ src/buckets/miniapp/helpers/RenderAppBluePrintHelper.js
@@ -1,34 +1,26 @@
 import React from 'react';
 import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
 import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
+import MakerDaoLandingBluePrint from '../../../dapps/makerdao/screens/MakerDaoLandingBluePrint';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 const colorScheme = Appearance.getColorScheme();
 const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
 
-function MakerDaoLandingBluePrint() {
-  const products = [
-    {
-      id: 1,
-      product_name: 'Leverage Up ETH',
-      component: 'LeverageEthProduct',
-    },
-    {
-      id: 2,
-      product_name: 'Borrow Stablecoin DAI',
-      component: 'BorrowDaiProduct',
-    },
-  ];
-
-  return (
-    <View style={styles.parent_view}>
-      <Text style={{color: 'white'}}>blueprint</Text>
-    </View>
-  );
+function RenderAppBluePrintHelper(props) {
+  if (props.function_name === 'MakerDaoLandingBluePrint') {
+    return <MakerDaoLandingBluePrint />;
+  } else {
+    return (
+      <View style={styles.parent_view}>
+        <Text style={{color: 'white'}}>nothing</Text>
+      </View>
+    );
+  }
 }
 
-export default MakerDaoLandingBluePrint;
+export default RenderAppBluePrintHelper;
 
 const styles = StyleSheet.create({
   parent_view: {
--- src/buckets/miniapp/screens/MiniAppLanding.js
+++ src/buckets/miniapp/screens/MiniAppLanding.js
@@ -4,6 +4,7 @@ import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
 import FastImage from 'react-native-fast-image';
 import {SharedElement} from 'react-native-shared-element';
 import HeaderBlock from '../components/HeaderBlock';
+import RenderAppBluePrintHelper from '../helpers/RenderAppBluePrintHelper';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
@@ -12,12 +13,17 @@ const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
 
 function MiniAppLanding({route}) {
   const {app_details} = route.params;
+
+  console.log(app_details.landing_blueprint_function_name);
+
   return (
     <View style={styles.parent_view}>
       <SharedElement id={`item.${app_details.app_name}.app_icon`}>
         <HeaderBlock app_details={app_details} style={{top: 0}} />
       </SharedElement>
-      <Text>...</Text>
+      <RenderAppBluePrintHelper
+        function_name={app_details.landing_blueprint_function_name}
+      />
     </View>
   );
 }
