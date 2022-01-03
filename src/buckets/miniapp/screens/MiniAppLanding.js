28840857fbb75a0af731f9609c5669651ce98103
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-04 02:30:53 +0530

    Maker Dao app - swipe setup - now changed to react native snap carousel

--- src/buckets/miniapp/screens/MiniAppLanding.js
+++ src/buckets/miniapp/screens/MiniAppLanding.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Appearance,
+  ScrollView,
+} from 'react-native';
 import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
 import FastImage from 'react-native-fast-image';
 import {SharedElement} from 'react-native-shared-element';
@@ -14,23 +21,27 @@ const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
 function MiniAppLanding({route}) {
   const {app_details} = route.params;
 
-  console.log(app_details.landing_blueprint_function_name);
-
   return (
-    <View style={styles.parent_view}>
-      <SharedElement id={`item.${app_details.app_name}.app_icon`}>
-        <HeaderBlock app_details={app_details} style={{top: 0}} />
-      </SharedElement>
-      <RenderAppBluePrintHelper
-        function_name={app_details.landing_blueprint_function_name}
-      />
-    </View>
+    <ScrollView style={styles.parent_scroll_view}>
+      <View style={styles.parent_view}>
+        <SharedElement id={`item.${app_details.app_name}.app_icon`}>
+          <HeaderBlock app_details={app_details} style={{top: 0}} />
+        </SharedElement>
+        <RenderAppBluePrintHelper
+          function_name={app_details.landing_blueprint_function_name}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
 export default MiniAppLanding;
 
 const styles = StyleSheet.create({
+  parent_scroll_view: {
+    flex: 1,
+    backgroundColor: themeHere.colors.background,
+  },
   parent_view: {
     flex: 1,
     alignItems: 'center',
--- src/dapps/makerdao/screens/MakerDaoLandingBluePrint.js
+++ src/dapps/makerdao/screens/MakerDaoLandingBluePrint.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Appearance,
+  ScrollView,
+} from 'react-native';
 import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
-import SwipeableProductsShowcase from '../../../bits/SwipeableProductsShowcase';
+import LeverageEthProduct from '../producttiles/LeverageEthProduct';
+import BorrowDaiProduct from '../producttiles/BorrowDaiProduct';
+import Swiper from 'react-native-swiper';
+import Carousel from 'react-native-snap-carousel';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
@@ -9,6 +19,28 @@ const colorScheme = Appearance.getColorScheme();
 const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
 
 function MakerDaoLandingBluePrint() {
+  const [currentViewIndex, setCurrentViewIndex] = useState(0);
+
+  function changeCurrentViewIndex(index) {
+    setCurrentViewIndex(index);
+  }
+
+  function ProductHeader() {
+    if (currentViewIndex === 0) {
+      return (
+        <View style={styles.product_head}>
+          <Text style={styles.product_title}>Leverage ETH</Text>
+        </View>
+      );
+    } else if (currentViewIndex === 1) {
+      return (
+        <View style={styles.product_head}>
+          <Text style={styles.product_title}>Borrow Stablecoin DAI</Text>
+        </View>
+      );
+    }
+  }
+
   const products = [
     {
       id: 1,
@@ -22,10 +54,38 @@ function MakerDaoLandingBluePrint() {
     },
   ];
 
+  function RenderProduct({item, index}) {
+    console.log(index + 'index');
+    if (index === 0) {
+      return (
+        <View style={styles.product_head}>
+          <Text style={styles.product_title}>Leverage ETH</Text>
+        </View>
+      );
+    } else if (index === 1) {
+      return (
+        <View style={styles.product_head}>
+          <Text style={styles.product_title}>Borrow Stablecoin DAI</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.product_head}>
+          <Text style={styles.product_title}>gone crazy</Text>
+        </View>
+      );
+    }
+  }
+
   return (
     <View style={styles.parent_view}>
-      <Text style={{color: 'white'}}>blueprint</Text>
-      <SwipeableProductsShowcase products_list={products} />
+      <ProductHeader />
+      <Carousel
+        data={products}
+        renderItem={RenderProduct}
+        sliderWidth={windowWidth}
+        itemWidth={windowWidth}
+      />
     </View>
   );
 }
@@ -36,5 +96,18 @@ const styles = StyleSheet.create({
   parent_view: {
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  swiper_wrapper: {
+    backgroundColor: '#EEEEEE50',
+    height: windowHeight * 0.5,
+  },
+  product_head: {
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  product_title: {
+    ...themeHere.text.header_bold,
+    color: themeHere.colors.foreground,
+    textAlign: 'center',
   },
 });
