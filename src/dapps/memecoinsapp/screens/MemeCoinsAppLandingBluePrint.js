16439d7f84ee72c43630b9d651efd7805bb90a41
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-04 17:33:56 +0530

    Uniswap and Memecoins app - setup folders and blueprint

---
+++ src/dapps/memecoinsapp/screens/MemeCoinsAppLandingBluePrint.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Appearance,
+  ScrollView,
+} from 'react-native';
+import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
+import LeverageEthProduct from '../products/leverageeth/LeverageEthProduct';
+import BorrowDaiProduct from '../products/borrowdai/BorrowDaiProduct';
+import Carousel from 'react-native-snap-carousel';
+import {SquircleView} from 'react-native-figma-squircle';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+const colorScheme = Appearance.getColorScheme();
+const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
+
+function MemeCoinsAppLandingBluePrint() {
+  const products = [
+    {
+      id: 1,
+      product_name: 'Currently Trending Tokens',
+      component: 'LeverageEthProduct',
+    },
+  ];
+
+  function RenderProductMakerDao({item, index}) {
+    if (index === 0) {
+      return (
+        <View style={styles.product_view}>
+          <Text style={styles.product_title}>Leverage ETH</Text>
+          <SquircleView
+            squircleParams={{
+              cornerSmoothing: 1,
+              cornerRadius: 15,
+              fillColor: themeHere.colors.mid_ground + '25',
+            }}
+            style={styles.product_tile_view}>
+            <LeverageEthProduct />
+          </SquircleView>
+        </View>
+      );
+    } else if (index === 1) {
+      return (
+        <View style={styles.product_view}>
+          <Text style={styles.product_title}>Borrow Stablecoin DAI</Text>
+          <SquircleView
+            squircleParams={{
+              cornerSmoothing: 1,
+              cornerRadius: 15,
+              fillColor: themeHere.colors.mid_ground + '25',
+            }}
+            style={styles.product_tile_view}>
+            <BorrowDaiProduct />
+          </SquircleView>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.product_view}>
+          <Text style={styles.product_title}></Text>
+        </View>
+      );
+    }
+  }
+
+  return (
+    <View style={styles.parent_view}>
+      <Carousel
+        data={products}
+        renderItem={RenderProductMakerDao}
+        sliderWidth={windowWidth}
+        itemWidth={windowWidth}
+      />
+    </View>
+  );
+}
+
+export default MemeCoinsAppLandingBluePrint;
+
+const styles = StyleSheet.create({
+  parent_view: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  product_view: {
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  product_title: {
+    ...themeHere.text.header_bold,
+    color: themeHere.colors.foreground,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  product_tile_view: {
+    width: windowWidth - 40,
+  },
+});
