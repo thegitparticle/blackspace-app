cf27a2d4df10b2409407a6fb2140f35746742651
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-06 16:28:34 +0530

    Market Prices redux setup - api url added

--- src/redux/MarketPricesActions.js
+++ src/redux/MarketPricesActions.js
@@ -6,8 +6,11 @@ export const GetMarketPrices = () => {
     let res = [];
 
     axios
-      .get('https://run.mocky.io/v3/66004f3d-e25e-43e8-a95e-a44a5a614508')
+      .get(
+        'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cbinancecoin%2Csolana%2Cusd-coin%2Ccardano%2Cripple%2Cpolkadot%2Cterra-luna%2Cavalanche-2%2Cdogecoin%2Cshiba-inu%2Cmatic-network%2Cbinance-usd%2Ccrypto-com-chain%2Cchainlink%2Cterrausd%2Ccosmos%2Calgorand%2Clitecoin%2Cnear%2Cdai%2Cbitcoin-cash%2Cuniswap%2Ctron%2Cfantom%2Cokb%2Cstellar%2Cinternet-computer&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true',
+      )
       .then(response => (res = response.data))
+      .then(data => console.log(data))
       .then(() =>
         dispatch({
           type: ADD_MARKETPRICES,
