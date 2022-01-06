c7955683f45d9040c20cbdc8bf0433997fceb7b0
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-06 22:21:21 +0530

    Market Prices - api link change

--- src/redux/MarketPricesActions.js
+++ src/redux/MarketPricesActions.js
@@ -7,7 +7,7 @@ export const GetMarketPrices = () => {
 
     axios
       .get(
-        'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cbinancecoin%2Csolana%2Cusd-coin%2Ccardano%2Cripple%2Cpolkadot%2Cterra-luna%2Cavalanche-2%2Cdogecoin%2Cshiba-inu%2Cmatic-network%2Cbinance-usd%2Ccrypto-com-chain%2Cchainlink%2Cterrausd%2Ccosmos%2Calgorand%2Clitecoin%2Cnear%2Cdai%2Cbitcoin-cash%2Cuniswap%2Ctron%2Cfantom%2Cokb%2Cstellar%2Cinternet-computer&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true',
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false',
       )
       .then(response => {
         // (res = response.data)
