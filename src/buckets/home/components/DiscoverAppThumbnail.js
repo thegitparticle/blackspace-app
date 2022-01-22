b07120cc987633614a31ebcc325d62ed9c00f640
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-22 23:46:54 +0530

    Discover Apps thumbnails - styles fix

--- src/buckets/home/components/DiscoverAppThumbnail.js
+++ src/buckets/home/components/DiscoverAppThumbnail.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Appearance,
+  TouchableOpacity,
+} from 'react-native';
 import {
   ButterThemeDark,
   ButterThemeLight,
 } from '../../../../../../Desktop/soupapp/src/theme/ButterTheme';
 import FastImage from 'react-native-fast-image';
+import {useNavigation} from '@react-navigation/native';
+import {SharedElement} from 'react-native-shared-element';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 const colorScheme = Appearance.getColorScheme();
 const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
+
+const width = (windowWidth - 80) / 3;
 
 function DiscoverAppThumbnail(app_details: {
   app_id: number,
@@ -17,19 +28,31 @@ function DiscoverAppThumbnail(app_details: {
   app_name: string,
   extra_message: string,
 }) {
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.parent_view} key={app_details.app_id}>
-      <FastImage
-        source={{
-          uri: app_details.app_icon,
-          priority: FastImage.priority.normal,
-        }}
-        resizeMode={FastImage.resizeMode.contain}
-        style={styles.app_image}
-      />
-      <Text style={styles.title}>{app_details.app_name}</Text>
-      <Text style={styles.subtitle}>{app_details.extra_message}</Text>
-    </View>
+    <TouchableOpacity
+      style={styles.parent_view}
+      key={app_details.app_id.toString()}
+      onPress={() =>
+        navigation.navigate('MiniAppLanding', {
+          app_details,
+        })
+      }>
+      <SharedElement id={`item.${app_details.app_name}.app_icon`}>
+        <FastImage
+          source={{
+            uri: app_details.app_icon,
+            priority: FastImage.priority.normal,
+          }}
+          resizeMode={FastImage.resizeMode.cover}
+          style={styles.app_image}
+        />
+      </SharedElement>
+      <View style={styles.text_view}>
+        <Text style={styles.title}>{app_details.app_name}</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
@@ -37,15 +60,25 @@ export default DiscoverAppThumbnail;
 
 const styles = StyleSheet.create({
   parent_view: {
-    width: (windowWidth - 80) / 3,
-    height: ((windowWidth - 80) / 3) * 1.5,
+    width: width,
+    height: width * 1.5,
     backgroundColor: themeHere.colors.off_background,
     flexDirection: 'column',
     borderRadius: 15,
+    flexWrap: 'wrap',
+    marginVertical: 5,
   },
   app_image: {
-    width: (windowWidth - 80) / 3,
-    height: ((windowWidth - 80) / 3) * 1.125,
+    width: width,
+    height: width * 1.125,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  text_view: {
+    width: width,
+    height: width * 0.375,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     ...themeHere.text.body_medium,
