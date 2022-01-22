7a59387e8c4033d0db875b3eaade6b891211e844
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-22 19:08:29 +0530

    tailwind css setup

--- App.js
+++ App.js
@@ -16,6 +16,8 @@ import {ModalPortal} from 'react-native-modals';
 import codePush from 'react-native-code-push';
 import FlashMessage from 'react-native-flash-message';
 import {LogBox} from 'react-native';
+import {TailwindProvider} from 'tailwind-rn';
+import utilities from './tailwind.json';
 
 const App: () => Node = () => {
   LogBox.ignoreLogs([
@@ -25,14 +27,16 @@ const App: () => Node = () => {
   return (
     <Provider store={storehere}>
       <PersistGate loading={null} persistor={persistor}>
-        <RootStack />
-        <ModalPortal />
-        <FlashMessage
-          position="top"
-          duration={3000}
-          textStyle={{fontFamily: 'GothamRounded-Medium', fontSize: 15}}
-          titleStyle={{fontFamily: 'GothamRounded-Medium', fontSize: 15}}
-        />
+        <TailwindProvider utilities={utilities}>
+          <RootStack />
+          <ModalPortal />
+          <FlashMessage
+            position="top"
+            duration={3000}
+            textStyle={{fontFamily: 'GothamRounded-Medium', fontSize: 15}}
+            titleStyle={{fontFamily: 'GothamRounded-Medium', fontSize: 15}}
+          />
+        </TailwindProvider>
       </PersistGate>
     </Provider>
   );
