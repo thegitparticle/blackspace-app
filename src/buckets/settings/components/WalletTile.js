7ead55f882dfe39e6b85f2bdb4f22f2f30d62943
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-23 17:40:54 +0530

    Settings -> walletTile - dripsy styles

--- src/buckets/settings/components/WalletTile.js
+++ src/buckets/settings/components/WalletTile.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
   Dimensions,
   Appearance,
   TouchableOpacity,
-  Image,
 } from 'react-native';
+import {Text, View, Image} from 'dripsy';
 import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
 import Iconly from '../../../miscsetups/customfonts/Iconly';
 
@@ -17,67 +15,98 @@ const colorScheme = Appearance.getColorScheme();
 const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
 
 function WalletTile() {
-  function DefaultCrypto() {
-    return (
-      <TouchableOpacity style={styles.listitem_view}>
-        <View style={styles.listitem_leftside_view}>
-          <Image
-            style={styles.listitem_icon}
-            source={require('../../../../assets/crypto_bitcoin_icon.png')}
-          />
-          <Text style={styles.listitem_title}>Wallet Default Crypto</Text>
-        </View>
-        <Iconly
-          name="ChevronRightBold"
-          color={themeHere.colors.foreground}
-          size={25}
-        />
-      </TouchableOpacity>
-    );
-  }
-
   function BaseFiatCurrency() {
     return (
-      <TouchableOpacity style={styles.listitem_view}>
-        <View style={styles.listitem_leftside_view}>
-          <Image
-            style={styles.listitem_icon}
-            source={require('../../../../assets/dollar_icon.png')}
+      <TouchableOpacity>
+        <View
+          sx={{
+            height: 75,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+          variant="layout.sub_view_50_margin">
+          <View
+            sx={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+            }}>
+            <Image
+              variant="images.small_icon_25_round"
+              source={require('../../../../assets/dollar_icon.png')}
+            />
+            <Text
+              variant="subhead_medium"
+              sx={{marginHorizontal: '$5', color: 'foreground'}}>
+              Base Currency
+            </Text>
+          </View>
+          <Iconly
+            name="ChevronRightBold"
+            color={themeHere.colors.foreground}
+            size={25}
           />
-          <Text style={styles.listitem_title}>Base Currency</Text>
         </View>
-        <Iconly
-          name="ChevronRightBold"
-          color={themeHere.colors.foreground}
-          size={25}
-        />
       </TouchableOpacity>
     );
   }
 
   function WalletBackup() {
     return (
-      <TouchableOpacity style={styles.listitem_view}>
-        <View style={styles.listitem_leftside_view}>
-          <Image
-            style={styles.listitem_icon}
-            source={require('../../../../assets/icloud_icon.png')}
+      <TouchableOpacity>
+        <View
+          sx={{
+            height: 75,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+          variant="layout.sub_view_50_margin">
+          <View
+            sx={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+            }}>
+            <Image
+              variant="images.small_icon_25_round"
+              source={require('../../../../assets/icloud_icon.png')}
+            />
+            <Text
+              variant="subhead_medium"
+              sx={{marginHorizontal: '$5', color: 'foreground'}}>
+              iCloud Backup
+            </Text>
+          </View>
+          <Iconly
+            name="ChevronRightBold"
+            color={themeHere.colors.foreground}
+            size={25}
           />
-          <Text style={styles.listitem_title}>iCloud Backup</Text>
         </View>
-        <Iconly
-          name="ChevronRightBold"
-          color={themeHere.colors.foreground}
-          size={25}
-        />
       </TouchableOpacity>
     );
   }
 
   return (
-    <View style={styles.parent_view}>
-      <Text style={styles.title_text}>WALLET</Text>
-      <DefaultCrypto />
+    <View
+      sx={{
+        backgroundColor: 'off_background',
+        borderRadius: 15,
+        marginBottom: '$6',
+        alignItems: 'center',
+      }}
+      variant="layout.sub_view_20_margin">
+      <Text
+        variant="subhead_medium"
+        sx={{
+          paddingVertical: '$5',
+          color: 'foreground',
+          opacity: 0.25,
+        }}>
+        WALLET
+      </Text>
       <BaseFiatCurrency />
       <WalletBackup />
     </View>
