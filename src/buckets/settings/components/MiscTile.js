f0da227e52112f27db8e91ee18e89fe5691300b2
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-23 17:47:58 +0530

    Settings -> Misc Tile - dripsy styles

--- src/buckets/settings/components/MiscTile.js
+++ src/buckets/settings/components/MiscTile.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
   Dimensions,
   Appearance,
   TouchableOpacity,
-  Image,
 } from 'react-native';
+import {Text, View, Image} from 'dripsy';
 import {ButterThemeDark, ButterThemeLight} from '../../../theme/ButterTheme';
 import Iconly from '../../../miscsetups/customfonts/Iconly';
 import {connect} from 'react-redux';
@@ -22,64 +20,133 @@ let state_here = {};
 function MiscTile() {
   function PrivacyPolicy() {
     return (
-      <TouchableOpacity style={styles.listitem_view}>
-        <View style={styles.listitem_leftside_view}>
-          <Image
-            style={styles.listitem_icon}
-            source={require('../../../../assets/eye_icon.png')}
+      <TouchableOpacity>
+        <View
+          sx={{
+            height: 75,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+          variant="layout.sub_view_50_margin">
+          <View
+            sx={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+            }}>
+            <Image
+              variant="images.small_icon_25_round"
+              source={require('../../../../assets/eye_icon.png')}
+            />
+            <Text
+              variant="subhead_medium"
+              sx={{marginHorizontal: '$5', color: 'foreground'}}>
+              Privacy Policy
+            </Text>
+          </View>
+          <Iconly
+            name="ChevronRightBold"
+            color={themeHere.colors.foreground}
+            size={25}
           />
-          <Text style={styles.listitem_title}>Privacy Policy</Text>
         </View>
-        <Iconly
-          name="ChevronRightBold"
-          color={themeHere.colors.foreground}
-          size={25}
-        />
       </TouchableOpacity>
     );
   }
 
   function TermsAndConditions() {
     return (
-      <TouchableOpacity style={styles.listitem_view}>
-        <View style={styles.listitem_leftside_view}>
-          <Image
-            style={styles.listitem_icon}
-            source={require('../../../../assets/defender_icon.png')}
+      <TouchableOpacity>
+        <View
+          sx={{
+            height: 75,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+          variant="layout.sub_view_50_margin">
+          <View
+            sx={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+            }}>
+            <Image
+              variant="images.small_icon_25_round"
+              source={require('../../../../assets/defender_icon.png')}
+            />
+            <Text
+              variant="subhead_medium"
+              sx={{marginHorizontal: '$5', color: 'foreground'}}>
+              Terms & Conditions
+            </Text>
+          </View>
+          <Iconly
+            name="ChevronRightBold"
+            color={themeHere.colors.foreground}
+            size={25}
           />
-          <Text style={styles.listitem_title}>Terms & Conditions</Text>
         </View>
-        <Iconly
-          name="ChevronRightBold"
-          color={themeHere.colors.foreground}
-          size={25}
-        />
       </TouchableOpacity>
     );
   }
 
   function Logout() {
     return (
-      <TouchableOpacity style={styles.listitem_view}>
-        <View style={styles.listitem_leftside_view}>
-          <Image
-            style={styles.listitem_icon}
-            source={require('../../../../assets/logout_icon.png')}
+      <TouchableOpacity>
+        <View
+          sx={{
+            height: 75,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+          variant="layout.sub_view_50_margin">
+          <View
+            sx={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+            }}>
+            <Image
+              variant="images.small_icon_25_round"
+              source={require('../../../../assets/logout_icon.png')}
+            />
+            <Text
+              variant="subhead_medium"
+              sx={{marginHorizontal: '$5', color: 'foreground'}}>
+              Logout
+            </Text>
+          </View>
+          <Iconly
+            name="ChevronRightBold"
+            color={themeHere.colors.foreground}
+            size={25}
           />
-          <Text style={styles.listitem_title}>Logout</Text>
         </View>
-        <Iconly
-          name="ChevronRightBold"
-          color={themeHere.colors.foreground}
-          size={25}
-        />
       </TouchableOpacity>
     );
   }
 
   return (
-    <View style={styles.parent_view}>
-      <Text style={styles.title_text}>MISC</Text>
+    <View
+      sx={{
+        backgroundColor: 'off_background',
+        borderRadius: 15,
+        marginBottom: '$6',
+        alignItems: 'center',
+      }}
+      variant="layout.sub_view_20_margin">
+      <Text
+        variant="subhead_medium"
+        sx={{
+          paddingVertical: '$5',
+          color: 'foreground',
+          opacity: 0.25,
+        }}>
+        MISC
+      </Text>
       <PrivacyPolicy />
       <TermsAndConditions />
       <Logout />
