1a2fd7e16fe4fc4f615ba9d066ace4f573195d75
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-26 13:10:47 +0530

    Erc token balances - redux setup - added static url - mainnet alchemy issue

--- src/redux/appcore/MyTokenBalancesActions.js
+++ src/redux/appcore/MyTokenBalancesActions.js
@@ -6,10 +6,13 @@ export const GetTokenBalances = wallet_address => {
     let res = [];
 
     axios
+      // .get(
+      //   'https://suprblack.xyz/api/users/get_erc20_token_balance/' +
+      //     wallet_address +
+      //     '/',
+      // )
       .get(
-        'https://suprblack.xyz/api/users/get_erc20_token_balance/' +
-          wallet_address +
-          '/',
+        'https://suprblack.xyz/api/users/get_erc20_token_balance/0xc5F59709974262c4AFacc5386287820bDBC7eB3A/',
       )
       .then(response => {
         res = response.data;
