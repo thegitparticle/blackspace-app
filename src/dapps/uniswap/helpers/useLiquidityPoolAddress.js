e96307415c77ac5ce9f49c2de31db38ff1b85e0c
Set <12975157+thegitparticle@users.noreply.github.com>
2022-01-28 12:43:20 +0530

    LP Address and details fetch from useLPAddress hook - working

--- src/dapps/uniswap/helpers/useLiquidityPoolAddress.js
+++ src/dapps/uniswap/helpers/useLiquidityPoolAddress.js
@@ -1,19 +1,60 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import _ from 'lodash';
 
 export default function useLiquidityPoolAddress(token0Address, token1Address) {
   const [loadingLPAddress, setLoadingLPAddress] = useState(true);
   const [lpAddress, setLPAddress] = useState('');
 
-  if (token0Address < token1Address) {
-    console.log(
-      token0Address + 'is smaller than' + token1Address + 'token0 big',
-    );
-  } else {
-    console.log(
-      token1Address + 'is smaller than' + token0Address + 'token1 big',
-    );
+  let token0Here = '';
+  let token1Here = '';
+
+  if (token0Address > token1Address) {
+    token0Here = token1Address;
+    token1Here = token0Address;
   }
+
+  const data = JSON.stringify({
+    query: `{
+    pools(orderBy: feeTier, where: {
+        token0: "${token0Here}",
+        token1: "${token1Here}"}) {
+      id
+      tick
+      sqrtPrice
+      feeTier
+      liquidity
+      token0Price
+      token1Price
+    }
+  }`,
+    variables: {token0: token0Here, token1: token1Here},
+  });
+
+  const config = {
+    method: 'post',
+    url: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
+
+  const fetchInfo = () => {
+    axios(config)
+      .then(function (response) {
+        setLPAddress(_.last(response.data.data.pools));
+        console.log(_.last(response.data.data.pools) + 'lp');
+        setLoadingLPAddress(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, [token0Address, token1Address]);
 
   return {loadingLPAddress, lpAddress};
 }
