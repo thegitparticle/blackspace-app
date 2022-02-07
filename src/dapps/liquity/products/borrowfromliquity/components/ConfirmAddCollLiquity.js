c76725e3fc8b4e3cbe4f2e281876419d9a3c2b68
Set <12975157+thegitparticle@users.noreply.github.com>
2022-02-08 02:47:50 +0530

    Liquity - Add Collateral - transaction modal setup and logic

---
+++ src/dapps/liquity/products/borrowfromliquity/components/ConfirmAddCollLiquity.js
@@ -0,0 +1,266 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
+import {
+  ButterThemeDark,
+  ButterThemeLight,
+} from '../../../../../theme/ButterTheme';
+import LinearGradient from 'react-native-linear-gradient';
+import {Button} from 'react-native-elements';
+import Compound from '@compound-finance/compound-js';
+import {ETH_NETWORK} from 'react-native-dotenv';
+import EmojiIcon from '../../../../../bits/EmojiIcon';
+import TokenWithIconBadge from '../../../../../bits/TokenWithIconBadge';
+import {useNavigation} from '@react-navigation/native';
+import useEthFiatPrice from '../../../../../helpers/useGetEthFiatPrice';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+const colorScheme = Appearance.getColorScheme();
+const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
+
+/*
+          ChangeBody={changeBodyToTransaction}
+          State={state_here}
+          CollateralNeededEth={collateralNeededEth}
+ */
+
+function ConfirmAddCollLiquity(props) {
+  const navigation = useNavigation();
+  const {loadingEth, priceEth} = useEthFiatPrice();
+
+  const [renderContext, setRenderContext] = useState('Checking');
+  /*
+  All render states: Checking | WalletHasAmount | WalletHasNoETHButERCs | NoAmount
+   */
+
+  let ethBalanceInWallet =
+    Number(props.State.MyProfileReducer.myProfileDetails.eth_balance) *
+    10 ** -18;
+
+  function checkIfWalletHasBalance() {
+    if (Number(props.CollateralNeededEth) < Number(ethBalanceInWallet)) {
+      setRenderContext('WalletHasAmount');
+    } else {
+      if (
+        Number(props.CollateralNeededEth) * Number(priceEth) <
+        Number(props.State.MyProfileReducer.myProfileDetails.portfolio_value)
+      ) {
+        setRenderContext('WalletHasNoETHButERCs');
+      } else {
+        setRenderContext('NoAmount');
+      }
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletHasBalance();
+  }, []);
+
+  function MainBlock() {
+    if (renderContext === 'Checking') {
+      return (
+        <View style={styles.main_block_view}>
+          <EmojiIcon
+            color={themeHere.colors.mid_ground + '50'}
+            size={80}
+            emoji={'⌛'}
+          />
+          <Text style={styles.text_highlighted}>
+            checking your wallet for balances
+          </Text>
+        </View>
+      );
+    } else if (renderContext === 'WalletHasAmount') {
+      return (
+        <View style={styles.main_block_view}>
+          <EmojiIcon
+            color={themeHere.colors.success_green_dark}
+            size={80}
+            emoji={'👍'}
+          />
+          <Text style={styles.text_highlighted}>
+            Your wallet has enough ETH as collateral
+          </Text>
+        </View>
+      );
+    } else if (renderContext === 'WalletHasNoETHButERCs') {
+      return (
+        <View style={styles.main_block_view}>
+          <EmojiIcon
+            color={themeHere.colors.danger_red}
+            size={80}
+            emoji={'⚠️'}
+          />
+          <Text style={styles.text_highlighted}>
+            you do not have enough ETH but have the needed collateral in tokens.
+          </Text>
+        </View>
+      );
+    } else if (renderContext === 'NoAmount') {
+      return (
+        <View style={styles.main_block_view}>
+          <EmojiIcon
+            color={themeHere.colors.danger_red}
+            size={80}
+            emoji={'⚠️'}
+          />
+          <Text style={styles.text_highlighted}>
+            your wallet does have {props.CollateralNeededEth} ETH as collateral,
+            reduce collateral amount and try again
+          </Text>
+        </View>
+      );
+    } else {
+      return <View />;
+    }
+  }
+
+  function ButtonBlock() {
+    if (renderContext === 'Checking') {
+      return (
+        <View style={styles.button_block_view}>
+          <Button
+            title={'go back'}
+            type={'solid'}
+            onPress={() => navigation.goBack()}
+            containerStyle={styles.next_button_container}
+            buttonStyle={styles.next_button_style}
+            titleStyle={styles.next_button_title}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: [themeHere.colors.mid_ground + '50'],
+            }}
+          />
+        </View>
+      );
+    } else if (renderContext === 'WalletHasAmount') {
+      return (
+        <View style={styles.button_block_view}>
+          <Button
+            title={'confirm borrow'}
+            type={'solid'}
+            onPress={() => props.ChangeBody()}
+            containerStyle={styles.next_button_container}
+            buttonStyle={styles.next_button_style}
+            titleStyle={styles.next_button_title}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: [
+                themeHere.colors.success_green_dark,
+                themeHere.colors.success_green,
+              ],
+            }}
+          />
+        </View>
+      );
+    } else if (renderContext === 'WalletHasNoETHButERCs') {
+      return (
+        <View style={styles.button_block_view}>
+          <Button
+            title={'convert to ETH on Uniswap'}
+            type={'solid'}
+            onPress={() => props.ChangeBody()}
+            containerStyle={styles.next_button_container}
+            buttonStyle={styles.next_button_style}
+            titleStyle={styles.next_button_title}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: [themeHere.colors.pink, themeHere.colors.pink + '90'],
+            }}
+          />
+        </View>
+      );
+    } else if (renderContext === 'NoAmount') {
+      return (
+        <View style={styles.button_block_view}>
+          <Button
+            title={'go back'}
+            type={'solid'}
+            onPress={() => navigation.goBack()}
+            containerStyle={styles.next_button_container}
+            buttonStyle={styles.next_button_style}
+            titleStyle={styles.next_button_title}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: [
+                themeHere.colors.danger_red_dark,
+                themeHere.colors.danger_red,
+              ],
+            }}
+          />
+        </View>
+      );
+    } else {
+      return <View />;
+    }
+  }
+
+  return (
+    <View style={styles.parent_view}>
+      <View />
+      <MainBlock />
+      <ButtonBlock />
+    </View>
+  );
+}
+
+export default ConfirmAddCollLiquity;
+
+const styles = StyleSheet.create({
+  parent_view: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  main_block_view: {
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  enter_amount_text: {
+    ...themeHere.text.title_1,
+    color: themeHere.colors.foreground,
+  },
+  text_highlighted: {
+    ...themeHere.text.header,
+    color: themeHere.colors.foreground,
+    marginVertical: 30,
+    maxWidth: windowWidth * 0.7,
+    textAlign: 'center',
+    lineHeight: 30,
+  },
+  text_not_highlighted: {
+    ...themeHere.text.body_medium,
+    color: themeHere.colors.foreground + '75',
+    marginVertical: 30,
+    maxWidth: windowWidth * 0.7,
+    textAlign: 'center',
+    lineHeight: 30,
+  },
+  unsupported_coins_context_suggestions_view: {
+    flexDirection: 'column',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  supported_coins_view: {
+    marginVertical: 15,
+    flexDirection: 'row',
+  },
+  button_block_view: {
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  next_button_container: {
+    marginVertical: 30,
+    alignSelf: 'center',
+  },
+  next_button_style: {
+    width: windowWidth * 0.5,
+    height: 50,
+    borderRadius: 25,
+  },
+  next_button_title: {
+    ...themeHere.text.body_medium,
+    color: 'white',
+  },
+});
