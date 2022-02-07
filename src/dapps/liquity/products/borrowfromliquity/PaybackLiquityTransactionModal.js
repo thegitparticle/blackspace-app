b00ffc3bdc9a60e19989193757fbefdb5dc3fba2
Set <12975157+thegitparticle@users.noreply.github.com>
2022-02-08 03:06:19 +0530

    Liquity - Payback Debt - transaction modal setup and loic

---
+++ src/dapps/liquity/products/borrowfromliquity/PaybackLiquityTransactionModal.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
+import {ButterThemeDark, ButterThemeLight} from '../../../../theme/ButterTheme';
+import {connect} from 'react-redux';
+import ConfirmAddCollLiquity from './components/ConfirmAddCollLiquity';
+import TransactionOngoingAddCollLiquity from './components/TransactionOngoingAddCollLiquity';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+const colorScheme = Appearance.getColorScheme();
+const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
+
+let state_here = {};
+
+function PaybackLiquityTransactionModal({route, dispatch}) {
+  const {collAmount, borrowAmount} = route.params;
+
+  const [renderScreen, setRenderScreen] = useState('ConfirmBorrow');
+
+  function RenderBody() {
+    if (renderScreen === 'TransactionOngoing') {
+      return (
+        <TransactionOngoingAddCollLiquity
+          ChangeBody={changeBodyToConfirmBorrow}
+          State={state_here}
+          CollateralNeededEth={collAmount}
+          BorrowAmount={borrowAmount}
+        />
+      );
+    } else if (renderScreen === 'ConfirmBorrow') {
+      return (
+        <ConfirmAddCollLiquity
+          ChangeBody={changeBodyToTransaction}
+          State={state_here}
+          CollateralNeededEth={collAmount}
+          BorrowAmount={borrowAmount}
+        />
+      );
+    } else {
+      return <></>;
+    }
+  }
+
+  function changeBodyToTransaction() {
+    setRenderScreen('TransactionOngoing');
+  }
+
+  function changeBodyToConfirmBorrow() {
+    setRenderScreen('ConfirmBorrow');
+  }
+
+  return (
+    <View style={styles.parent_view}>
+      {/*<ModalGoBackHeader title={`Borrow ${info.cToken[0].underlying_symbol}`} />*/}
+      <RenderBody />
+    </View>
+  );
+}
+
+const mapStateToProps = state => {
+  state_here = state;
+  return state_here;
+};
+
+export default connect(mapStateToProps)(PaybackLiquityTransactionModal);
+
+const styles = StyleSheet.create({
+  parent_view: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: themeHere.colors.background,
+  },
+});
