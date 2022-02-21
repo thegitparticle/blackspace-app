2382a2789ea452b4c9eaf0748f11e63903bdf52b
Set <12975157+thegitparticle@users.noreply.github.com>
2022-02-21 20:04:47 +0530

    Info ToolTip component setup in Bits

---
+++ src/bits/InfoIcon.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Dimensions, Appearance} from 'react-native';
+import {ButterThemeDark, ButterThemeLight} from '../theme/ButterTheme';
+import {Icon, Tooltip} from 'react-native-elements';
+import {Bounceable} from 'rn-bounceable';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+const colorScheme = Appearance.getColorScheme();
+
+const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
+
+function InfoIcon(props) {
+  // size, information
+
+  return (
+    <Tooltip
+      popover={
+        <Text style={{color: 'white', ...themeHere.text.caption}}>
+          {props.information}
+        </Text>
+      }
+      overlayColor={'transparent'}
+      backgroundColor={'#333333'}
+      withPointer={true}>
+      <View style={{padding: 10}}>
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: props.size,
+            height: props.size,
+            borderRadius: props.size / 2,
+            backgroundColor: themeHere.colors.foreground,
+          }}>
+          <Icon
+            name="info"
+            type="entypo"
+            color={themeHere.colors.background}
+            size={props.size / 2}
+          />
+        </View>
+      </View>
+    </Tooltip>
+  );
+}
+
+export default InfoIcon;
