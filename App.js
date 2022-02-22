94780695e9c0bdfa135a3626d896fa52a45ff956
Set <12975157+thegitparticle@users.noreply.github.com>
2022-02-22 20:48:58 +0530

    External service setup for production

--- App.js
+++ App.js
@@ -23,15 +23,15 @@ import {Amplitude} from '@amplitude/react-native';
 
 import * as Sentry from '@sentry/react-native';
 
-// dev mode
-Sentry.init({
-  dsn: 'https://75dfb14196fb466a87028e482a9113bc@o578195.ingest.sentry.io/6212337',
-});
-
-//production
+// // dev mode
 // Sentry.init({
-//   dsn: "https://9230dc56e7754eab81d37474629416e5@o578195.ingest.sentry.io/6219940",
+//   dsn: 'https://75dfb14196fb466a87028e482a9113bc@o578195.ingest.sentry.io/6212337',
 // });
+
+// production
+Sentry.init({
+  dsn: 'https://9230dc56e7754eab81d37474629416e5@o578195.ingest.sentry.io/6219940',
+});
 
 const App: () => Node = () => {
   LogBox.ignoreLogs([
@@ -73,9 +73,9 @@ const App: () => Node = () => {
 
   // Amplitude analytics setup
   const ampInstance = Amplitude.getInstance();
-  ampInstance.init('3f8238f4e3a8c083393f5a5c86631f75'); // dev mode
+  // ampInstance.init('3f8238f4e3a8c083393f5a5c86631f75'); // dev mode
 
-  // ampInstance.init('de072a1eacb5ba8927a6362092a50d50');     // production
+  ampInstance.init('de072a1eacb5ba8927a6362092a50d50'); // production
 
   return (
     <Provider store={storehere}>
