81902537f4b0aafea8b2dd7557f992383ca0ebb7
thegitparticle <maddamsettysanjit@gmail.com>
2022-06-08 10:12:22 +0530

    removed typescript plugins needed for pt libraries

--- src/buckets/main/transactions/screens/SimpleEthereumTxnScreen.js
+++ src/buckets/main/transactions/screens/SimpleEthereumTxnScreen.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {StyleSheet, Dimensions, Appearance} from 'react-native';
 import {View, Text} from 'dripsy';
 import {ButterThemeDark, ButterThemeLight} from '../../../../theme/ButterTheme';
+import {connect} from 'react-redux';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 const colorScheme = Appearance.getColorScheme();
 const themeHere = colorScheme === 'dark' ? ButterThemeDark : ButterThemeLight;
 
+let state_here = {};
+
+// transaction to send ETH from one wallet to another
 function SimpleEthereumTxnScreen() {
   return (
     <View sx={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -16,4 +20,9 @@ function SimpleEthereumTxnScreen() {
   );
 }
 
-export default SimpleEthereumTxnScreen;
+const mapStateToProps = state => {
+  state_here = state;
+  return state_here;
+};
+
+export default connect(mapStateToProps)(SimpleEthereumTxnScreen);
